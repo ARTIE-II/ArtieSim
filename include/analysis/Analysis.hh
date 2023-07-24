b1d33d81233e92b10ef4f0bf0f58d5d73fbc17fd
Junying Huang <xxzdkg@gmail.com>
2023-07-24 12:05:10 -0700

    update analysis code

--- include/analysis/Analysis.hh
+++ include/analysis/Analysis.hh
@@ -1,6 +1,5 @@
 /**
  * @file Analysis.hh
- * @author Yashwanth Bezawada [ysbezawada@ucdavis.edu]
  * @author Nicholas Carrara [nmcarrara@ucdavis.edu]
  * @brief 
  * @version 0.1
@@ -13,23 +12,15 @@ namespace Artie
 {
     struct AnalysisTuple
     {
-        G4double x_pos = 0;
-        G4double y_pos = 0;
-        G4double z_pos = 0;
-        G4double n_energy = 0;
-        G4int N_x = 0;
-        G4int N_y = 0;
-        G4int N_z = 0;
-        G4double n_tof = 0;
-        G4int n_track_id = 0;
-        G4double x_tracking = 0;
-        G4double y_tracking = 0;
-        G4double z_tracking = 0;
+        G4int event_id = 0;
+        G4int number_of_primaries = 0;
+        G4double alpha_energy = 0;
+        G4double triton_energy = 0;
+        G4double neutron_init_energy = 0;
+        G4double neutron_tof = 0;
     };
-
     void AnalysisFunctionRunBegin();
     void AnalysisFunctionRunEnd();
     void AnalysisFunctionEventBegin();
     void AnalysisFunctionEventEnd();
-
 }
